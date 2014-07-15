fdba97228931b632190e1aee4ce476866ce1232d
yashutanna <tanna.yashu@gmail.com>
2014-07-15 20:27:49 +0200

    made use of openCV libraries

--- Dot Slash/Dot Slash/Dot Slash/Program.cs
+++ Dot Slash/Dot Slash/Dot Slash/Program.cs
@@ -34,8 +34,8 @@ namespace Dot_Slash
 			Console.WriteLine("Image processing options:");
 			Console.WriteLine("1) Resizing to 300 X 200");
 			Console.WriteLine("2) apply gaussian filter");
-			Console.WriteLine("3) edge Detection");
-			Console.WriteLine("4) grey scaling");
+			Console.WriteLine("3) grey scaling");
+			Console.WriteLine("4) edge Detection");
 			Console.WriteLine("5) generate integral image");
 			Console.WriteLine("6) crop images");
 			Console.WriteLine("7) filter images by hand");
@@ -69,16 +69,16 @@ namespace Dot_Slash
 						}
 					case 3:
 						{
-							Console.WriteLine("Applying Edge Detector");
-							imageProcessor.detectEdges();
-							Console.WriteLine("Done Edge Detection");
+							Console.WriteLine("Changing images to greyscale");
+							imageProcessor.makeGreyscale();
+							Console.WriteLine("Done greyscaling");
 							break;
 						}
 					case 4:
 						{
-							Console.WriteLine("Changing images to greyscale");
-							imageProcessor.makeGreyscale();
-							Console.WriteLine("Done greyscaling");
+							Console.WriteLine("Applying Edge Detector");
+							imageProcessor.detectEdges();
+							Console.WriteLine("Done Edge Detection");
 							break;
 						}
 					case 5:
@@ -295,93 +295,96 @@ namespace Dot_Slash
 
 		public virtual void execute()
 		{
-			String[] pictures = Directory.GetFiles(imagePath, "*.jpg", SearchOption.TopDirectoryOnly);
 			bool isExists = System.IO.Directory.Exists("Edged/");
 			if (!isExists)
 				System.IO.Directory.CreateDirectory("Edged/");
 			bool gausExists = System.IO.Directory.Exists("Gaussian/");
 			if (gausExists)
 				imagePath = "Gaussian/";
+			String[] pictures = Directory.GetFiles(imagePath, "*.jpg", SearchOption.TopDirectoryOnly);
 			for (int i = 0; i < pictures.Length; i++)
 			{
 				Bitmap img = new Bitmap(pictures[i]);
-				Bitmap edgedImg = new Bitmap(img.Width, img.Height);
-				makeEdge(img, edgedImg);
+				Image<Gray, Byte> edgedImg;
+				edgedImg = makeEdge(img);
 				edgedImg.Save("Edged/Edged_" + new FileInfo(pictures[i]).Name);
 				Console.WriteLine(imagePath+ "-> Edged/Edged_" + new FileInfo(pictures[i]).Name);
 			}
 		}
 
-		public void makeEdge(Bitmap img, Bitmap edge)
+		public Image<Gray, Byte> makeEdge(Bitmap img)
 		{
+			Image<Gray, Byte> image = new Image<Gray, Byte> (img);
+			image = image.Canny(39, 60);
+			return image;
 			/**
 			 * white colour for edge and black for any other pixel
 			 * */
-			Color black = Color.FromArgb(255, 0, 0, 0);
-			Color white = Color.FromArgb(255, 255, 255, 255);
-			for (int i = 1; i < img.Width - 1; i++)
-			{
-				for (int j = 1; j < img.Height - 1; j++)
-				{
-					//get RGB values for current pixel
-					Color color = img.GetPixel(i, j);
-					byte r, g, b;
-					r = color.R;
-					g = color.G;
-					b = color.B;
-					//get RGB Values for pixel directly right of current pixel
-					Color colorRight = img.GetPixel(i, j + 1);
-					byte rR, gR, bR;
-					rR = colorRight.R;
-					gR = colorRight.G;
-					bR = colorRight.B;
-					//get RGB values for pixel directly under current pixel
-					Color colorBottom = img.GetPixel(i + 1, j);
-					byte rB, gB, bB;
-					rB = colorBottom.R;
-					gB = colorBottom.G;
-					bB = colorBottom.B;
-					//get RGB values for pixel diagonally to the bottom right of the current pixel
-					Color colorBottomRight = img.GetPixel(i + 1, j + 1);
-					byte rBR, gBR, bBR;
-					rBR = colorBottomRight.R;
-					gBR = colorBottomRight.G;
-					bBR = colorBottomRight.B;
+			//Color black = Color.FromArgb(255, 0, 0, 0);
+			//Color white = Color.FromArgb(255, 255, 255, 255);
+			//for (int i = 1; i < img.Width - 1; i++)
+			//{
+			//	for (int j = 1; j < img.Height - 1; j++)
+			//	{
+			//		//get RGB values for current pixel
+			//		Color color = img.GetPixel(i, j);
+			//		byte r, g, b;
+			//		r = color.R;
+			//		g = color.G;
+			//		b = color.B;
+			//		//get RGB Values for pixel directly right of current pixel
+			//		Color colorRight = img.GetPixel(i, j + 1);
+			//		byte rR, gR, bR;
+			//		rR = colorRight.R;
+			//		gR = colorRight.G;
+			//		bR = colorRight.B;
+			//		//get RGB values for pixel directly under current pixel
+			//		Color colorBottom = img.GetPixel(i + 1, j);
+			//		byte rB, gB, bB;
+			//		rB = colorBottom.R;
+			//		gB = colorBottom.G;
+			//		bB = colorBottom.B;
+			//		//get RGB values for pixel diagonally to the bottom right of the current pixel
+			//		Color colorBottomRight = img.GetPixel(i + 1, j + 1);
+			//		byte rBR, gBR, bBR;
+			//		rBR = colorBottomRight.R;
+			//		gBR = colorBottomRight.G;
+			//		bBR = colorBottomRight.B;
 
-					/**the difference between the RGB values of each pixel are calculated and raised to the power of 2 respectively to get the
-					 * absolute difference between values. 
-					 **/
-					double differenceRight = (Math.Pow((r - rR), 2) + Math.Pow((g - gR), 2) + Math.Pow((b - bR), 2));
-					double differenceBottom = (Math.Pow((r - rB), 2) + Math.Pow((g - gB), 2) + Math.Pow((b - bB), 2));
-					double differenceBottomRight = 0.0;
-					if (diagonal)
-					{
-						differenceBottomRight = (Math.Pow((r - rBR), 2) + Math.Pow((g - gBR), 2) + Math.Pow((b - bBR), 2));
-					}
+			//		/**the difference between the RGB values of each pixel are calculated and raised to the power of 2 respectively to get the
+			//		 * absolute difference between values. 
+			//		 **/
+			//		double differenceRight = (Math.Pow((r - rR), 2) + Math.Pow((g - gR), 2) + Math.Pow((b - bR), 2));
+			//		double differenceBottom = (Math.Pow((r - rB), 2) + Math.Pow((g - gB), 2) + Math.Pow((b - bB), 2));
+			//		double differenceBottomRight = 0.0;
+			//		if (diagonal)
+			//		{
+			//			differenceBottomRight = (Math.Pow((r - rBR), 2) + Math.Pow((g - gBR), 2) + Math.Pow((b - bBR), 2));
+			//		}
 
-					//instead of rooting the distance between the colour values, we rather raise the threshold to the power of 2 as well
-					Double thresholdSquared = Math.Pow(threshold, 2);
+			//		//instead of rooting the distance between the colour values, we rather raise the threshold to the power of 2 as well
+			//		Double thresholdSquared = Math.Pow(threshold, 2);
 
-					//if bottom right pixel was used to determine whether or not the pixel is an edge
-					if (diagonal)
-					{
-						/**
-						 * If either the right, bottom, or bottom right differs from the current pixel by some threshold, then the pixel is on an edge
-						 */
-						if (differenceBottom > thresholdSquared || differenceRight > thresholdSquared || differenceBottomRight > thresholdSquared)
-							edge.SetPixel(i, j, white);
-						else
-							edge.SetPixel(i, j, black);
-					}
-					else
-					{
-						if (differenceBottom > thresholdSquared || differenceRight > thresholdSquared)
-							edge.SetPixel(i, j, white);
-						else
-							edge.SetPixel(i, j, black);
-					}
-				}
-			}
+			//		//if bottom right pixel was used to determine whether or not the pixel is an edge
+			//		if (diagonal)
+			//		{
+			//			/**
+			//			 * If either the right, bottom, or bottom right differs from the current pixel by some threshold, then the pixel is on an edge
+			//			 */
+			//			if (differenceBottom > thresholdSquared || differenceRight > thresholdSquared || differenceBottomRight > thresholdSquared)
+			//				edge.SetPixel(i, j, white);
+			//			else
+			//				edge.SetPixel(i, j, black);
+			//		}
+			//		else
+			//		{
+			//			if (differenceBottom > thresholdSquared || differenceRight > thresholdSquared)
+			//				edge.SetPixel(i, j, white);
+			//			else
+			//				edge.SetPixel(i, j, black);
+			//		}
+			//	}
+			//}
 		}
 
 	}
@@ -400,22 +403,22 @@ namespace Dot_Slash
 			bool isExists = System.IO.Directory.Exists("Gaussian/");
 			if (!isExists)
 				System.IO.Directory.CreateDirectory("Gaussian/");
-			bool resizedExists = System.IO.Directory.Exists("Resized/");
-			if (resizedExists)
-				imagePath = "Resized/";
 			String[] pictures = Directory.GetFiles(imagePath, "*.jpg", SearchOption.TopDirectoryOnly);
 			for (int i = 0; i < pictures.Length; i++)
 			{
 				Bitmap img = new Bitmap(pictures[i]);
-				Bitmap gaus = new Bitmap(img.Width, img.Height);
-				smooth(img, gaus);
+				Image<Bgra, Byte> gaus;
+				gaus = smooth(img);
 				gaus.Save("Gaussian/Filtered_" + new FileInfo(pictures[i]).Name);
 				Console.WriteLine(imagePath + "-> Gaussian/Filtered_" + new FileInfo(pictures[i]).Name);
 			}
 		}
 
-		public void smooth(Bitmap img, Bitmap gaus)
+		public Image<Bgra, Byte> smooth(Bitmap img)
 		{
+			Image<Bgra, Byte> smoothed = new Image<Bgra,Byte>(img);
+			smoothed = smoothed.SmoothGaussian(3);
+			return smoothed;
 			//The filter matrix we use for smoothing
 
 			/*		i-2	i-1	i	i+1	i+2
@@ -426,61 +429,61 @@ namespace Dot_Slash
 			*	j+2	2	4	5	4	2
 			*/
 			//int[,] filter = new int[5, 5] { { 2, 4, 5, 4, 2 }, { 4, 9, 12, 9, 4 }, { 5, 12, 15, 12, 5 }, { 4, 9, 12, 9, 4 }, { 2, 4, 5, 4, 2 } };
-				
-			int[,] filter = new int[3,3] {{1,2,1}, {2,4,2}, {1,2,1}};
-			//Loop through every pixel
-			for (int i = 2; i < img.Width - 2; i++)
-			{
-				for (int j = 2; j < img.Height - 2; j++)
-				{
-					int smoothValueR = 0;
-					int smoothValueG = 0;
-					int smoothValueB = 0;
-					int divisor = 0;
+			
+			//int[,] filter = new int[3,3] {{1,2,1}, {2,4,2}, {1,2,1}};
+			////Loop through every pixel
+			//for (int i = 2; i < img.Width - 2; i++)
+			//{
+			//	for (int j = 2; j < img.Height - 2; j++)
+			//	{
+			//		int smoothValueR = 0;
+			//		int smoothValueG = 0;
+			//		int smoothValueB = 0;
+			//		int divisor = 0;
 
-					//i = i coordinate of the pixel
-					//j = j coordinate of the pixel
+			//		//i = i coordinate of the pixel
+			//		//j = j coordinate of the pixel
 
-					//Loop around the current pixel
-					for (int x = -1; x <= 1; x++)
-					{
-						for (int y = -1; y <= 1; y++)
-						{
-							try
-							{
-								//newI & newJ = coordinate of the pixel around the current pixel we are smoothing
-								int newI = i + x;
-								int newJ = j + y;
+			//		//Loop around the current pixel
+			//		for (int x = -1; x <= 1; x++)
+			//		{
+			//			for (int y = -1; y <= 1; y++)
+			//			{
+			//				try
+			//				{
+			//					//newI & newJ = coordinate of the pixel around the current pixel we are smoothing
+			//					int newI = i + x;
+			//					int newJ = j + y;
 
-								//int r = (img.GetPixel(newI, newJ).R) * filter[x + 2, y + 2];	//to accomodate for x & y starting at -2 and filter indices only starting at [0,0]
-								//int g = (img.GetPixel(newI, newJ).G) * filter[x + 2, y + 2];
-								//int b = (img.GetPixel(newI, newJ).B) * filter[x + 2, y + 2];
+			//					//int r = (img.GetPixel(newI, newJ).R) * filter[x + 2, y + 2];	//to accomodate for x & y starting at -2 and filter indices only starting at [0,0]
+			//					//int g = (img.GetPixel(newI, newJ).G) * filter[x + 2, y + 2];
+			//					//int b = (img.GetPixel(newI, newJ).B) * filter[x + 2, y + 2];
 
-								int r = (img.GetPixel(newI, newJ).R) * filter[x + 1, y + 1];	//to accomodate for x & y starting at -2 and filter indices only starting at [0,0]
-								int g = (img.GetPixel(newI, newJ).G) * filter[x + 1, y + 1];
-								int b = (img.GetPixel(newI, newJ).B) * filter[x + 1, y + 1];
+			//					int r = (img.GetPixel(newI, newJ).R) * filter[x + 1, y + 1];	//to accomodate for x & y starting at -2 and filter indices only starting at [0,0]
+			//					int g = (img.GetPixel(newI, newJ).G) * filter[x + 1, y + 1];
+			//					int b = (img.GetPixel(newI, newJ).B) * filter[x + 1, y + 1];
 
-								smoothValueR += r;
-								smoothValueG += g;
-								smoothValueB += b;
-								divisor += filter[x + 1, y + 1];
-							}
-							catch
-							{
-								Console.WriteLine("Exception Caught");
-								//Pixel out of bounds - do nothing with this pixel
-							}
-						}
-					}
+			//					smoothValueR += r;
+			//					smoothValueG += g;
+			//					smoothValueB += b;
+			//					divisor += filter[x + 1, y + 1];
+			//				}
+			//				catch
+			//				{
+			//					Console.WriteLine("Exception Caught");
+			//					//Pixel out of bounds - do nothing with this pixel
+			//				}
+			//			}
+			//		}
 
-					int smoothedColorR = smoothValueR / divisor;
-					int smoothedColorG = smoothValueG / divisor;
-					int smoothedColorB = smoothValueB / divisor;
+			//		int smoothedColorR = smoothValueR / divisor;
+			//		int smoothedColorG = smoothValueG / divisor;
+			//		int smoothedColorB = smoothValueB / divisor;
 
-					Color newPixel = Color.FromArgb(smoothedColorR, smoothedColorG, smoothedColorB);
-					gaus.SetPixel(i, j, newPixel);
-				}
-			}
+			//		Color newPixel = Color.FromArgb(smoothedColorR, smoothedColorG, smoothedColorB);
+			//		gaus.SetPixel(i, j, newPixel);
+			//	}
+			//}
 		}
 	}
 
@@ -497,9 +500,9 @@ namespace Dot_Slash
 			bool isExists = System.IO.Directory.Exists("Greyscale/");
 			if (!isExists)
 				System.IO.Directory.CreateDirectory("Greyscale/");
-			bool gausExists = System.IO.Directory.Exists("Gaussian/");
+			bool gausExists = System.IO.Directory.Exists("Resized/");
 			if(gausExists)
-				imagePath = "Gaussian/";
+				imagePath = "Resized/";
 			String[] pictures = Directory.GetFiles(imagePath, "*.jpg", SearchOption.TopDirectoryOnly);
 
 			for (int i = 0; i < pictures.Length; i++)
@@ -516,8 +519,7 @@ namespace Dot_Slash
 
 		private Image<Gray, Byte> makeGreyScale(Bitmap originalImage)
 		{
-			Console.WriteLine("original: "+originalImage.PixelFormat);
-			Image<Bgra, Int32> c = new Image<Bgra, Int32>(originalImage);
+			Image<Bgra, Byte> c = new Image<Bgra, Byte>(originalImage);
 			Image<Gray, Byte> img = c.Convert<Gray, Byte>();
 			return img;
 			//Bitmap img = new Bitmap(originalImage.Width, originalImage.Height, System.Drawing.Imaging.PixelFormat.Format16bppGrayScale);
