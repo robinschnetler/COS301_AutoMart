d062995df62783db700dda01b7977e4fabe9a38a
yashutanna <tanna.yashu@gmail.com>
2014-09-23 15:30:58 +0200

    started integration

---
+++ Dot Slash/Dot Slash/Dot Slash/CarDetect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.Util;
+
+namespace Dot_Slash
+{
+	class CarDetect : Filter
+	{
+		int numNeighbours;
+		Size minSize, maxSize;
+		String classifier;
+
+	}
+}
---
+++ Dot Slash/Dot Slash/Dot Slash/Filter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization;
+using System.IO;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.Util;
+
+
+namespace Dot_Slash
+{
+	class Filter
+	{
+		Image<Bgr, Int32> input;
+		Image<Bgr, Int32> output;
+
+		public Filter(Image<Bgr, Int32> im)
+		{
+			input = im;
+		}
+	}
+}
