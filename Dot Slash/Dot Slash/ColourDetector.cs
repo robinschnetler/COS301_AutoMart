18c8ee021d4d64fdf4d156cfb2a1e462c9d7115d
Marko Sinovcic <markosinovcic@gmail.com>
2014-10-08 15:07:28 +0200

    traning automation

--- Dot Slash/Dot Slash/ColourDetector.cs
+++ Dot Slash/Dot Slash/ColourDetector.cs
@@ -17,7 +17,8 @@ namespace Dot_Slash
 {
     public class ColourDetector : Filter
     {
-        ArrayList colourBuckets; 
+        ArrayList colourBuckets;
+        ArrayList imageBlocks;
         public ColourDetector()
         {
             colourBuckets  = new ArrayList();
@@ -38,19 +39,199 @@ namespace Dot_Slash
             colourBuckets.Add(new ColourBucket("Bronze", 205, 127, 50));
             colourBuckets.Add(new ColourBucket("Charcoal", 51, 51, 51));
             //colourBuckets.Add(new ColourBucket("Beige", 245, 245, 220
+
+            imageBlocks = new ArrayList();
         }
 
         public void pump(ref AdvertDetails _advertDetails)
         {
-		if(!_advertDetails.CarFound)
-			_advertDetails.ExceptionList.Add("Car not found");
-		Bitmap img = _advertDetails.Image.ToBitmap().Clone(_advertDetails.Rect, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
-		int width = img.Width;
-		int height = img.Height;
-		String[] colours = loopThroughPixels(1, 0, colourBuckets, width, height, img);
-		_advertDetails.Colour1 = colours[0];
-		_advertDetails.Colour2 = colours[1];
-		_advertDetails.Colour3 = colours[2];
+		    if(!_advertDetails.CarFound)
+			    _advertDetails.ExceptionList.Add("Car not found");
+		    /*Bitmap img = _advertDetails.Image.ToBitmap().Clone(_advertDetails.Rect, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+		    int width = img.Width;
+		    int height = img.Height;
+		    String[] colours = loopThroughPixels(1, 0, colourBuckets, width, height, img);
+		    _advertDetails.Colour1 = colours[0];
+		    _advertDetails.Colour2 = colours[1];
+		    _advertDetails.Colour3 = colours[2];*/
+
+            String[] colour = getBlocks(_advertDetails);
+            _advertDetails.Colour1 = colour[0];
+            _advertDetails.Colour2 = colour[1];
+            _advertDetails.Colour3 = colour[2];
+
+        }
+
+        public String[] getBlocks(AdvertDetails _advertDetails)
+        {
+            int num_rows = 10;
+            int num_cols = 10;
+            int step_x = (int)(_advertDetails.Rect.Height / num_rows);
+            int step_y = (int)(_advertDetails.Rect.Width / num_cols);
+            int current_x = 0, current_y = 0;
+
+            double dots_treshold = 0.1;
+            int max_dots = (int)((_advertDetails.Rect.Height * _advertDetails.Rect.Width) * dots_treshold);
+
+            Bitmap edgedImage = drawEdge(_advertDetails.Image.ToBitmap());
+
+            while(true)
+            {
+                int num_dots = 0;
+                for (int x = current_x; x < current_x + step_x; x++)
+                {
+                    for (int y = current_y; y < current_y + step_y; y++)
+                    {
+                        if (edgedImage.GetPixel(x, y).ToArgb().Equals(Color.White.ToArgb()))
+                            num_dots++;
+
+                        if (num_dots > max_dots)
+                            goto Next;
+                    }
+                    imageBlocks.Add(new ImageBlock(current_x, current_y, step_x, step_y));
+                }
+
+            Next:
+                if (current_y + step_y < _advertDetails.Rect.Width)
+                {
+                    current_y += step_y;
+                }
+                else if (current_x + step_x < _advertDetails.Rect.Height)
+                {
+                    current_x += step_x;
+                    current_y = 0;
+                }
+                else
+                    goto Colour;
+            }
+
+        Colour:
+            List<List<ImageBlock>> groupedBlocks = new List<List<ImageBlock>>();
+            bool added = false;
+            foreach(ImageBlock block in imageBlocks)
+            {
+                block.Colours = getBlockColours(_advertDetails.Image.ToBitmap(), block);
+                added = false;
+   
+                foreach(List<ImageBlock> list in groupedBlocks)
+                {
+                    if(containsColour(list, block))
+                    {
+                        list.Add(block);
+                        added = true;
+                    }
+                }
+                if (!added)
+                    groupedBlocks.Add(new List<ImageBlock>() { block });
+            }
+
+            groupedBlocks.Sort(
+                delegate(List<ImageBlock> b1, List<ImageBlock> b2)
+                {
+                    return b1.Count.CompareTo(b2.Count);
+                }
+            );
+
+            return groupedBlocks.First().First().Colours;
+        }
+
+        public bool containsColour(List<ImageBlock> _list, ImageBlock _block)
+        {
+            if (_list.First().Colours[1].Equals(_block.Colours[1]))
+                return true;
+            else
+                return false;
+        }
+
+        public Bitmap drawEdge(Bitmap img)
+        {
+            Image<Gray, Byte> image = new Image<Gray, Byte>(img);
+            image = image.Canny(39, 60);
+            return image.ToBitmap();
+        }
+
+
+        public String[] getBlockColours(Bitmap _image, ImageBlock _block)
+        {
+            int pixelHop = 1;
+            int hueTolerance = 2;		//Should be in the range of 5 - 10
+            int saturationTolerance = 80;	//Should be approx 100
+            int valueTolerance = 150;	//Should be in the range of 170 - 200
+
+            int counter = 0;
+            int numColourBuckets = colourBuckets.Count;
+
+            int[] colourCounter = new int[numColourBuckets];
+
+            for (int i = 0; i < numColourBuckets; i++)
+            {
+                colourCounter[i] = 0;
+            }
+
+            foreach (ColourBucket currentBucket in colourBuckets)
+            {
+                for (int x = _block.X_coord; x < _block.X_coord + _block.Height; x++)
+                {
+                    for (int y = _block.Y_coord; y < _block.Y_coord + _block.Width; y += pixelHop)
+                    {
+                        Color clr = _image.GetPixel(x, y);
+
+                        double p_hue = 0.0, p_saturation = 0.0, p_value = 0.0;
+                        double bin_hue = currentBucket.h;
+                        double bin_saturation = currentBucket.s;
+                        double bin_value = currentBucket.v;
+                        convertRGBtoHSV(clr, out p_hue, out p_saturation, out p_value);
+                        if (inRange(p_hue, bin_hue, hueTolerance))						//Check if the hue is in range
+                            if (inRange(p_saturation * 255, bin_saturation * 255, saturationTolerance))	//Check if the saturation is in range
+                                if (inRange(p_value * 255, bin_value * 255, valueTolerance))		//Check if the value is in range
+                                    colourCounter[counter] = colourCounter[counter] + 1;
+                    }
+                }
+                counter++;
+            }
+
+            int max = -99999;
+            int index1 = 0;
+            for (int i = 0; i < colourCounter.Length; i++)
+            {
+                if (colourCounter[i] >= max)
+                {
+                    index1 = i;
+                    max = colourCounter[i];
+                }
+            }
+
+            ColourBucket dominantColour = (ColourBucket)colourBuckets[index1];
+            String firstMostCommon = dominantColour.colourName;
+
+            max = -99999;
+            int index2 = 0;
+            for (int i = 0; i < colourCounter.Length; i++)
+            {
+                if (colourCounter[i] >= max && i != index1)
+                {
+                    index2 = i;
+                    max = colourCounter[i];
+                }
+            }
+
+            dominantColour = (ColourBucket)colourBuckets[index2];
+            String secondMostCommon = dominantColour.colourName;
+
+            max = -99999;
+            int index3 = 0;
+            for (int i = 0; i < colourCounter.Length; i++)
+            {
+                if (colourCounter[i] >= max && i != index1 && i != index2)
+                {
+                    index3 = i;
+                    max = colourCounter[i];
+                }
+            }
+
+            dominantColour = (ColourBucket)colourBuckets[index3];
+            String thirdMostCommon = dominantColour.colourName;
+            return (new String[] { firstMostCommon, secondMostCommon, thirdMostCommon });
         }
 
         private String[] loopThroughPixels(int pixelHop, int numBinsToEliminate, ArrayList colourBuckets, int width, int height, Bitmap img)
